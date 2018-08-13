98b8adc43434cf778b32b1cdced934fc73fbf874
Wolfy Meier <wolfy@wlfy.it>
2018-08-13 01:08:59 -0700

    get packages

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+go get github.com/jinzhu/gorm
+go get github.com/juju/loggo
+go get gopkg.in/go-oauth2/redis.v1
+go get gopkg.in/oauth2.v3
+go get gopkg.in/session.v1
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o puphaus-api .
