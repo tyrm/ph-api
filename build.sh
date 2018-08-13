a5b1cbaa3aa3e684a18f7a6b0e6bf35c8493aaec
Wolfy Meier <wolfy@wlfy.it>
2018-08-13 01:18:29 -0700

    add more packages

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 go get github.com/codegangsta/inject
+go get github.com/gorilla/mux
 go get github.com/jinzhu/gorm
 go get github.com/juju/loggo
 go get github.com/lib/pq
+go get golang.org/x/crypto/bcrypt
 go get gopkg.in/go-oauth2/redis.v1
 go get gopkg.in/oauth2.v3
 go get gopkg.in/session.v1
