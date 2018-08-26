23946d3390455adfecffc6513204728630ac3088
Wolfy Meier <wolfy@wlfy.it>
2018-08-25 21:01:35 -0700

    preload users

--- models/client.go
+++ models/client.go
@@ -43,7 +43,7 @@ func (c *Client) GetUserID() string {
 }
 
 func GetClient(id string) (cli Client, err error) {
-	err = db.Where("client_id=?", id).First(&cli).Error
+	err = db.Preload("User").Where("client_id=?", id).First(&cli).Error
 	if err != nil {
 		logger.Errorf("GetClient(%s) Error: %s", id, err)
 	}
